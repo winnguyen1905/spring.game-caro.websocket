bc9ce14a22220b75194ae61f16defa3ccb14fcf2
unknown <nguyenthangloi19052004@gmail.com>
2024-08-30 22:57:53 +0700

    re-arch socket - split module

--- src/main/java/com/game/caro/socket/SocketBaseEventHandler.java
+++ src/main/java/com/game/caro/socket/SocketBaseEventHandler.java
@@ -1,9 +1,5 @@
-package com.game.caro.socket;
+package com.game.caro.socket; 
 
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.corundumstudio.socketio.AckRequest;
@@ -13,38 +9,21 @@ import com.corundumstudio.socketio.listener.ConnectListener;
 import com.corundumstudio.socketio.listener.DataListener;
 import com.corundumstudio.socketio.listener.DisconnectListener;
 import com.game.caro.model.MessageDTO;
-import com.game.caro.model.MessageDTO;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
-public class SocketServer {
+@RequiredArgsConstructor
+public class SocketBaseEventHandler {
 
     private final SocketIOServer server;
-
-    public SocketServer(SocketIOServer server) {
-        this.server = server;
-        server.addConnectListener(onUserConnectWithSocket);
-        server.addDisconnectListener(onUserDisconnectWithSocket);
-        server.addEventListener("send_message", MessageDTO.class, onSendMessage);
-    }
-
-    // Listen event `send_message`
-    public DataListener<MessageDTO> onSendMessage = new DataListener<MessageDTO>() {
-        @Override
-        public void onData(SocketIOClient client, MessageDTO message, AckRequest acknowledge) throws Exception {
-
-            // 3 param is : event name, send to ?, content
-            server.getBroadcastOperations().sendEvent("event", client, message);
-
-            // đã nhận được tin nhắn ? 
-            acknowledge.sendAckData("Message send to target user successfully");
-        }
-    };
+    private final SocketCoreEventHandler socketCoreEventHandler;
 
     // active when have new connect
     public ConnectListener onUserConnectWithSocket = new ConnectListener() {
         @Override
         public void onConnect(SocketIOClient client) {
-            System.out.println("onconnected " + client.getSessionId());
+            System.out.println(">> onconnected " + client.getSessionId());
         }
     };
 
@@ -52,7 +31,7 @@ public class SocketServer {
     public DisconnectListener onUserDisconnectWithSocket = new DisconnectListener() {
         @Override
         public void onDisconnect(SocketIOClient client) {
-            System.out.println("disconnected " + client.getSessionId());
+            System.out.println(">> disconnected " + client.getSessionId());
         }
     };
 
---
+++ src/main/java/com/game/caro/socket/SocketCoreEventHandler.java
@@ -0,0 +1,67 @@
+package com.game.caro.socket;
+
+import org.springframework.stereotype.Service;
+
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.DataListener;
+import com.game.caro.model.MessageDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SocketCoreEventHandler {
+
+    private final SocketIOServer server;
+
+    public DataListener<MessageDTO> onCreateRoom = new DataListener<MessageDTO>() {
+        @Override
+        public void onData(SocketIOClient client, MessageDTO message, AckRequest acknowledge) throws Exception {
+
+            // 3 param is : event name, send to ?, content
+            server.getBroadcastOperations().sendEvent("event", client, "cc t nhan duoc roi");
+
+            // đã nhận được tin nhắn ? 
+            acknowledge.sendAckData("Message send to target user successfully");
+        }
+    };
+
+    public DataListener<MessageDTO> onJoinRoom = new DataListener<MessageDTO>() {
+        @Override
+        public void onData(SocketIOClient client, MessageDTO message, AckRequest acknowledge) throws Exception {
+
+            // 3 param is : event name, send to ?, content
+            server.getBroadcastOperations().sendEvent("event", client, "cc t nhan duoc roi");
+
+            // đã nhận được tin nhắn ? 
+            acknowledge.sendAckData("Message send to target user successfully");
+        }
+    };
+
+    public DataListener<MessageDTO> onChat = new DataListener<MessageDTO>() {
+        @Override
+        public void onData(SocketIOClient client, MessageDTO message, AckRequest acknowledge) throws Exception {
+
+            // 3 param is : event name, send to ?, content
+            server.getBroadcastOperations().sendEvent("event", client, "cc t nhan duoc roi");
+
+            // đã nhận được tin nhắn ? 
+            acknowledge.sendAckData("Message send to target user successfully");
+        }
+    };
+
+    public DataListener<MessageDTO> onStartGamePlay = new DataListener<MessageDTO>() {
+        @Override
+        public void onData(SocketIOClient client, MessageDTO message, AckRequest acknowledge) throws Exception {
+
+            // 3 param is : event name, send to ?, content
+            server.getBroadcastOperations().sendEvent("event", client, "cc t nhan duoc roi");
+
+            // đã nhận được tin nhắn ? 
+            acknowledge.sendAckData("Message send to target user successfully");
+        }
+    };
+
+}
